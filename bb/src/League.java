976ad700e2ef0599cd8c6b5ffe874716bd69c949
KHM_DESKTOP\wiemo <wiemo@naver.com>
2017-01-10 23:17:46 +0900

    -리그, 팀 주석 추가

--- bb/src/League.java
+++ bb/src/League.java
@@ -6,15 +6,32 @@ import java.util.Scanner;
 
 public class League {
 
-	int year;
-	double BF, Hit, singles, doubles, triples, HR, BB, SO, HBP;
-	double leagueOVASingle, leagueOVADouble, leagueOVATriple, leagueOVAHR, leagueOVABB, leagueOVASO, leagueOVAHBP;
+	//리그 수치 스탯
+	int year;							//년도
+	double BF;							//상대타자수
+	double Hit;							//안타
+	double singles;						//단타
+	double doubles;						//2루타
+	double triples;						//3루타
+	double HR;							//홈런
+	double BB; 							//볼넷
+	double SO;							//삼진
+	double HBP;							//사구
+	
+	//리그평균스탯
+	double leagueOVASingle;				//리그평균단타율
+	double leagueOVADouble; 
+	double leagueOVATriple;
+	double leagueOVAHR;
+	double leagueOVABB; 
+	double leagueOVASO;
+	double leagueOVAHBP;
 
 	public League() { }
 
 	public void setLeagueOVA() {
-		leagueOVASingle = singles/BF;
-		leagueOVADouble = doubles/BF;
+		leagueOVASingle = singles/BF;	//리그 평균 단타율 = 단타/상대타자
+		leagueOVADouble = doubles/BF;	//리그 평균 2루타율 = 2루타 / 상대타자
 		leagueOVATriple = triples/BF;
 		leagueOVAHR = HR/BF;
 		leagueOVABB = BB/BF;
@@ -22,7 +39,7 @@ public class League {
 		leagueOVAHBP = HBP/BF;
 	}
 
-	public void setYear(int init_year) {
+	public void setYear(int init_year) {	//인수로 받아온 연도를 연도에 저장
 		year = init_year;
 	}
 
--- bb/src/Team.java
+++ bb/src/Team.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 
 public class Team {
 
-	String name;
-	int year;
-	ArrayList<Player> roster;
-	ArrayList<Player> bullpen;
+	String name;				//팀이름
+	int year;					//년도
+	ArrayList<Player> roster;	//팀 로스터, 플레이어 배열리스트
+	ArrayList<Player> bullpen;	//팀 불펜, 플레이어 배열리스트
 		
-	public Team() {
-		roster = new ArrayList<Player>();
-		bullpen = new ArrayList<Player>();
+	public Team() {				//팀 클래스 생성자
+		roster = new ArrayList<Player>();	//팀 로스터 초기화
+		bullpen = new ArrayList<Player>();	//팀 불펜 초기화
 	}
 
 	public void setInfo(String info) {
@@ -20,18 +20,18 @@ public class Team {
 		name = info.substring(9,(info.length()-5));
 	}
 
-	public void addPlayer(Player input) {	
-		roster.add(input);
-		if (input.position.equals("P"))
+	public void addPlayer(Player input) {	//선수추가 함수, 인수로 선수넣음
+		roster.add(input);					//로스터에 선수 추가함
+		if (input.position.equals("P"))		//만약 투수면 불펜에도 추가
 			bullpen.add(input);
 	}
 
-	public void zeroOut() {
+	public void zeroOut() {					//로스터에 있는 모든 선수들 게임스탯 초기화
 		for (Player p : roster)
 			p.clearGameStats();
 	}
 
-	public Player getPlayer(String name) {
+	public Player getPlayer(String name) {	//인수로 받은 이름의 선수를 리턴하는 함수
 		Player target = roster.get(0);
 		for (Player p : roster) {
 			if (p.name.equals(name))
@@ -40,12 +40,12 @@ public class Team {
 		return target;
 	}
 
-	public void setAverages() {
+	public void setAverages() {				//선수 스탯 평균계산
 		for (Player p : roster)
 			p.setAvg();
 	}
 
-	public String printRoster() {
+	public String printRoster() {			//로스터 내의 선수들을 출력함
 		String result = ""+year+" "+name+"\n";
 		result += "ROSTER:\n";
 		for (Player i : roster)
